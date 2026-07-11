55866c75160e09175ce4586855977c88746c42fb
Mostafa Shora <mm7326745@gmail.com>
2026-07-11 16:30:06 +0300

    Rename project from ToDoApp2 to ToDoApp and update related references

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ToDoApp2');
+  protected readonly title = signal('ToDoApp');
 }
